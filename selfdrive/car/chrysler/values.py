b34dd4065a2636b74b05844797458dda948dc59c
debugged-hosting <mbuboltz@debugged-hosting.com>
2021-02-02 12:35:46 -0600

    DELTA DOWN 2

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -7,7 +7,7 @@ Ecu = car.CarParams.Ecu
 class SteerLimitParams:
   STEER_MAX = 361 #512         # 262 faults
   STEER_DELTA_UP = 3      # 3 is stock. 100 is fine. 200 is too much it seems
-  STEER_DELTA_DOWN = 3    # no faults on the way down it seems
+  STEER_DELTA_DOWN = 2 #3    # no faults on the way down it seems
   STEER_ERROR_MAX = 80
 
 #   The start of turn is limited by delta up
