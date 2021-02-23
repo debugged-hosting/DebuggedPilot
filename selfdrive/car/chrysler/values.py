af52fe716b3039b6fafe6198563a46175ad3b7cd
debugged-hosting <mbuboltz@debugged-hosting.com>
2021-02-23 15:37:26 -0600

    put back to stock

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -6,8 +6,8 @@ Ecu = car.CarParams.Ecu
 
 class SteerLimitParams:
   STEER_MAX = 461         # 262 faults
-  STEER_DELTA_UP = 2      # 3 is stock. 100 is fine. 200 is too much it seems
-  STEER_DELTA_DOWN = 2    # no faults on the way down it seems
+  STEER_DELTA_UP = 3      # 3 is stock. 100 is fine. 200 is too much it seems
+  STEER_DELTA_DOWN = 3    # no faults on the way down it seems
   STEER_ERROR_MAX = 80
 
 
