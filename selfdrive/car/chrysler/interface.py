f5957b7a452adc8d28e0f93494d81a88069ee16b
debugged-hosting <mbuboltz@debugged-hosting.com>
2021-03-05 10:05:38 -0600

    back to indi

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -41,24 +41,24 @@ class CarInterface(CarInterfaceBase):
     # steerActuatorDelay is how far its looking ahead.
     # steerRateCost is how eager the steering is to make sudden changes.
 
-    #ret.lateralTuning.init('indi')
-    #ret.lateralTuning.indi.innerLoopGainBP = [0, 20]
-    #ret.lateralTuning.indi.innerLoopGainV = [4.0, 10.5]
-    #ret.lateralTuning.indi.outerLoopGainBP = [0, 20]
-    #ret.lateralTuning.indi.outerLoopGainV = [7.0, 11.5]
-    #ret.lateralTuning.indi.timeConstantBP = [0, 20]
-    #ret.lateralTuning.indi.timeConstantV = [0.5, 1.8]
-    #ret.lateralTuning.indi.actuatorEffectivenessBP = [0, 20]
-    #ret.lateralTuning.indi.actuatorEffectivenessV = [70.0, 75.0]
-    #ret.steerActuatorDelay = 0.8
+    ret.lateralTuning.init('indi')
+    ret.lateralTuning.indi.innerLoopGainBP = [0, 20]
+    ret.lateralTuning.indi.innerLoopGainV = [4.0, 10.5]
+    ret.lateralTuning.indi.outerLoopGainBP = [0, 20]
+    ret.lateralTuning.indi.outerLoopGainV = [7.0, 11.5]
+    ret.lateralTuning.indi.timeConstantBP = [0, 20]
+    ret.lateralTuning.indi.timeConstantV = [0.5, 1.8]
+    ret.lateralTuning.indi.actuatorEffectivenessBP = [0, 20]
+    ret.lateralTuning.indi.actuatorEffectivenessV = [70.0, 75.0]
+    ret.steerActuatorDelay = 0.8
 
     ### OLD PID TUNE - WORKED ON 0.7.7 ###
 
-    pidscale = 0.145
-    ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kfBP = [[9., 20.], [9., 20.], [0.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kfV = [[0.15 * pidscale,0.30 * pidscale], [0.03 * pidscale,0.05 * pidscale], [0.00006 * pidscale]] # full torque for 10 deg at 80mph means 0.00007818594
-    ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.1]]
-    ret.steerActuatorDelay = 0.02
+    #pidscale = 0.145
+    #ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kfBP = [[9., 20.], [9., 20.], [0.]]
+    #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kfV = [[0.15 * pidscale,0.30 * pidscale], [0.03 * pidscale,0.05 * pidscale], [0.00006 * pidscale]] # full torque for 10 deg at 80mph means 0.00007818594
+    #ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.1]]
+    #ret.steerActuatorDelay = 0.02
     
     ### MY PID TUNE - WORKS GOOD BUT JERKY ###
 
